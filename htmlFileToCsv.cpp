cdc74f62f4c2273f9a5d972c5c4478a9ebf4523b
Nick Cottrell <nrc1mr.e3@gmail.com>
2019-08-01 22:57:47 -0400

    created file importing function

--- htmlFileToCsv.cpp
+++ htmlFileToCsv.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <fstream>
 
-std::string readfile(std::string filename){
-    
+std::string readfile(std::string filename)
+{
+    std::string result = "";
+    ifstream src;
+    src.open(filename);
+    while (src.eof()) {
+        src >> "\n" >> result;
+    }
+    return result;
 }
