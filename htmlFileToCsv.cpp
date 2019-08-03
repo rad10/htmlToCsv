dc3f3dc3e665e3890c67f80a8ad5f1b55556e962
Nick Cottrell <nrc1mr.e3@gmail.com>
2019-08-03 12:02:10 -0400

    removed debugging comments

--- htmlFileToCsv.cpp
+++ htmlFileToCsv.cpp
@@ -15,21 +15,15 @@ string readfile(string filename)
 {
     string result, add;
     ifstream src(filename);
-    //cout << "file open: " << src.is_open() << "\nfileend: " << src.eof() << "\n";
     while (getline(src, add))
     {
         result += add + "\n";
     }
-    /* if (Debug)
-    {
-        cout << result << "\n";
-    } */
     src.close();
     return result;
 }
 string getContentSource(string file)
 {
-    //cout << readfile(file);
     stringstream lines(readfile(file));
     string text, sc;
     getline(lines, sc);
@@ -37,12 +31,7 @@ string getContentSource(string file)
     while (sc.find("<textarea") == -1 && !lines.eof())
     {
         getline(lines, sc);
-        //cout << l << ". " << sc << " | " << sc.find("<textarea") << "\n";
     }
-    /* if (Debug)
-    {
-        cout << sc << " | " << sc.find("<textarea") << "\n";
-    } */
     text = sc.substr(sc.find("<textarea") + 22, sc.length()) + "\n";
     getline(lines, sc);
     while (sc.find("</textarea>") == -1 && !lines.eof())
