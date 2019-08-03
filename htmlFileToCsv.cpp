89c53568a44b371ea0ec0d6a9607dfc46e7986da
Nick Cottrell <nrc1mr.e3@gmail.com>
2019-08-03 12:00:48 -0400

    successfully got the content out of the html

--- htmlFileToCsv.cpp
+++ htmlFileToCsv.cpp
@@ -9,27 +9,51 @@ struct database
     string name, timeIn, timeOut, purpose;
 };
 
-string
-readfile(string filename)
+bool Debug = true;
+
+string readfile(string filename)
 {
-    string result = "";
-    ifstream src;
-    src.open(filename);
-    while (src.eof())
+    string result, add;
+    ifstream src(filename);
+    //cout << "file open: " << src.is_open() << "\nfileend: " << src.eof() << "\n";
+    while (getline(src, add))
     {
-        src >> result;
-        result += "\n";
+        result += add + "\n";
     }
+    /* if (Debug)
+    {
+        cout << result << "\n";
+    } */
+    src.close();
     return result;
 }
-database getContentArray(string file)
+string getContentArray(string file)
 {
+    //cout << readfile(file);
     stringstream lines(readfile(file));
     string text, sc;
-    lines >> sc;
+    getline(lines, sc);
     /* gets me to the beginning of the line that i need */
-    while (sc.find("<textarea") != -1)
+    while (sc.find("<textarea") == -1 && !lines.eof())
     {
-        lines >> sc;
+        getline(lines, sc);
+        //cout << l << ". " << sc << " | " << sc.find("<textarea") << "\n";
     }
+    /* if (Debug)
+    {
+        cout << sc << " | " << sc.find("<textarea") << "\n";
+    } */
+    text = sc.substr(sc.find("<textarea") + 22, sc.length()) + "\n";
+    getline(lines, sc);
+    while (sc.find("</textarea>") == -1 && !lines.eof())
+    {
+        text += sc + "\n";
+        getline(lines, sc);
+    }
+    text += sc.substr(0, sc.find("</textarea>"));
+    return text;
+}
+int main()
+{
+    cout << getContentArray("sheet1.html");
 }
